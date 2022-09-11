b8e9b8a927890f381c6ed87b13bc1d1897fd0453
LBuluma <lawrencebuluma@gmail.com>
2022-09-11 23:27:33 +0300

    new services

---
+++ src/main/java/com/webapp/dao/HelpServiceDao.java
@@ -0,0 +1,9 @@
+package com.webapp.dao;
+
+public class HelpServiceDao {
+
+	public HelpServiceDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/webapp/serviceimpl/HelpIssueServiceImpl.java
@@ -0,0 +1,30 @@
+package com.webapp.serviceimpl;
+
+import java.util.List;
+
+import com.webapp.model.HelpIssue;
+import com.webapp.service.HelpIssueService;
+
+public class HelpIssueServiceImpl implements HelpIssueService{
+
+	@Override
+	public List<HelpIssue> getIssueCategories(int moduleId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNewHelpIssue(int moduleId, HelpIssue helpIssue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public HelpIssue getHelpIssue(int helpIssueId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
